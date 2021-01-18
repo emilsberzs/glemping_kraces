b4671d64b7629b3f16b8452964f25f66a187d496
emilsberzs <70856334+emilsberzs@users.noreply.github.com>
2021-01-18 20:42:08 +0000

    Added post to admin site, created PublishedManager

--- glemping/models.py
+++ glemping/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.utils import timezone
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -15,6 +20,9 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=20,
                               choices=STATUS_CHOICES, default='draft')
+    image = models.ImageField(blank=True)
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ('-publish',)
