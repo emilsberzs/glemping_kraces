9f121f10cc17017216d79decae02ff2aa0af08e8
emilsberzs <70856334+emilsberzs@users.noreply.github.com>
2021-01-20 19:18:02 +0000

    Long overdue commit. Mostly sorted out base layout

--- glemping/views.py
+++ glemping/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+                  'glemping/news/list.html',
+                  {'posts': posts, 'section': 'news'})
+
+
+def post_detail(request, post):
+    post = get_object_or_404(Post, slug=post, status='published')
+    return render(request, 'glemping/news/detail.html', {'post': post})
